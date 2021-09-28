e04ab3cd96c1beba4af7a67c3fa83e73c66e1642
Your Name <Your Github Email Address>
2021-09-28 14:21:33 +0000

    Solved all the problems

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 //1
-function countToFifty(){
-  
+const countToFifty = () => {
+    for(let i = 1; i < 51; i++) {
+        console.log(i);
+    }
 }
 
 //2
-function countFromOne(){
-  
+
+const countFromOne = (int) => {
+    for(let i = 1; i < int + 1; i++) {
+        console.log(i);
+    }
+    
 }
+  
+
 
 //3
-function threeAndFiveInFifty(){
-  
+const threeAndFiveInFifty = () => {
+    for(let i = 1; i < 51; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
 }
 
 //4
-function threeAndFive(){
-  
+
+const threeOrFive = (int) => {
+      for(let i = 1; i < int + 1; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
+    
+}
+//5
+
+const triple = array => {
+    let tripledArray = [];
+    for(let i = 0; i < array.length; i++) {
+        let tripledValue = array[i] * 3;
+        
+        tripledArray.push(tripledValue);
+    } return tripledArray; 
 }
 
 
+
+//countToFifty(); 
+//countFromOne(25)
+
+//threeAndFiveInFifty()
+
+//threeOrFive(15);
+
+//console.log(triple([1, 2, 3]));
